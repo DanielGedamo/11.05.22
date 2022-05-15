98b4d07cf9cb36364d23c11dddbefa7bae8e2420
DANIELGEDAMO <danielgadamo@gmail.com>
2022-05-15 18:43:49 +0300

    k

---
+++ main.js
@@ -0,0 +1,96 @@
+
+function hello (){
+ var i =0
+ while(i<3){
+     console.log('hello world')
+     i++
+ }}
+hello()
+function max (num1,num2){
+if(num1<=num2){
+    return (num2)
+}
+else{
+    return (num1)
+}  
+}
+// max(4,9)
+function sum(num1,num2){
+result=(num1+num2)
+return (result)
+}
+sum(5,5)
+function sum(num){
+
+}
+function print (){
+    document.write('daniel')
+}
+print()
+function city(){
+    console.log('rehovot')
+}
+city()
+function aler (){
+    alert('wellcom')
+}
+aler()
+function over(){
+    var  age =confirm ('are you over 18?')
+
+if (age){
+    document.write('hello')
+}
+else{
+    document.write('byebye')
+}
+}
+over()
+
+function user (){
+     alert('enter name')
+     var 
+}
+user()
+function name10 (){
+    for(var i=1; i<=10; i++){
+        console.log('daniel');
+    }
+}
+name10()
+function numTo100(){
+    for(i=0; i<=100; i++){
+        console.log(i);
+    }
+}
+numTo100()
+function finel(num1,num2){
+    for(i=0; i<1; i++){
+        result=((num1+num2)/2)
+        document.write(result )
+    }
+}
+finel(9,867)
+function width(){
+result=(num1*num2)
+
+}
+// finel(5,5)
+function avgi (num1,){
+    avg=((num1+prompt)/4)
+}
+avgi(5)
+function fornum(){
+    +prompt("enter 3 Number");{
+        document.write(avg)
+    }
+
+function MinAndMax(){
+for(i=1; i>0;i++){
+   var i=+prompt("enter number")
+   var max=Math.max
+   document.write(max)
+}
+
+}
+MinAndMax()
